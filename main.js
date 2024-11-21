582152a67d81ba866293efa1b11a2dfde14beb90
Hael <michelkleinth@gmail.com>
2024-11-20 22:04:16 -0300

    adição do texto riscado

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 
-
 $(document).ready(function() {
+    $('<div></div>').appendTo('form')
     $('form').on('submit', function(e) {
         e.preventDefault();
         const respostaFormulario = $('#name-task').val()
-        const novoItem = $(`<p></p>`)
-        $(`<li">${respostaFormulario}</li>`).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
+        const novoItem = $(`<ul></ul>`)
+        $(`<li>${respostaFormulario}</li>`).appendTo(novoItem);
+        $(novoItem).appendTo('div')
         $('#name-task').val('')
+
+    });
+    $('div').click(function(){
+        $('li').css('text-decoration', 'line-through');
     })
 })
